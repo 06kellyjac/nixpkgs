e665a8065dc3bd10a0761948021c684faa0d7623
06kellyjac <dev@j-k.io>
2026-05-19 14:47:37 +0100

    qwen3-tts-cpp: add extra samples feature

--- pkgs/by-name/qw/qwen3-tts-cpp/package.nix
+++ pkgs/by-name/qw/qwen3-tts-cpp/package.nix
@@ -34,6 +34,7 @@ stdenv.mkDerivation (finalAttrs: {
   patches = [
     ./0001-local-deps.patch
     ./0002-use-updated-httplib.patch
+    ./0003-predefined-samples-server.patch
   ];
 
   nativeBuildInputs = [
