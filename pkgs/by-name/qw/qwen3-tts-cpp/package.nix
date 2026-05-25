df33a6aa203eba1758c1f1deb89e59beb70c7ad8
06kellyjac <dev@j-k.io>
2026-05-25 11:35:31 +0100

    qwen3-tts-cpp: gguf based voice bank

--- pkgs/by-name/qw/qwen3-tts-cpp/package.nix
+++ pkgs/by-name/qw/qwen3-tts-cpp/package.nix
@@ -34,7 +34,8 @@ stdenv.mkDerivation (finalAttrs: {
   patches = [
     ./0001-local-deps.patch
     ./0002-use-updated-httplib.patch
-    ./0003-predefined-samples-server-icl4.patch
+    ./0003-predefined-samples-server-icl4.8.patch
+    # ./0003-predefined-samples-server-icl9.patch
   ];
 
   nativeBuildInputs = [
